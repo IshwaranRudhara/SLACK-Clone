f45a5f4c3589a31de6b6243c9593676a76fa03c1
Ishwaran <ishwaranrudhara@gmail.com>
2023-02-23 12:00:35 +0530

    ping

---
+++ script.js
@@ -0,0 +1,32 @@
+// var a = 3;
+// var b;
+// if ((a = b)) {
+//   console.log(b);
+// }
+
+// let a = 5;
+// let b = 6;
+// if (a == b) {
+//   console.log("YES IT IS EQUAL");
+// } else {
+//   console.log("no its not equal");
+// }
+
+// if else statement.
+
+// var a = 1;
+// var b = 2;
+// if (a > b) {
+//   console.log("i am print :" + a);
+// } else {
+//   console.log("it's not equal ");
+// }
+
+for (i = 0; i < 2; i++) {
+  for (j = 0; j < 2; i++) {
+    for (k = 0; k < 2; k++) {
+      // console.log();
+    }
+  }
+  //   console.log(i);
+}
